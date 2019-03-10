ba5e984037da84edb53936d3ea1aeba2abebc3c9
paulo jansen <pauloo.jansen@gmail.com>
2019-03-10 18:06:40 -0300

    adding show number to text request workin

--- http_server.py
+++ http_server.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
+from number_to_text import number_to_text
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "i"
+@app.route('/<number>')
+def show_number_to_text(number):
+    parser = number_to_text.NumberToText()
+    result = parser.number_to_text(int(number))
+    json_result = jsonify({ "extenso": result })
+    return json_result
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
