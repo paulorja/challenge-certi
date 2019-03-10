e7cfae81bfa5af1b35cb4a1cfc59242b82b944d5
paulo jansen <pauloo.jansen@gmail.com>
2019-03-10 18:33:32 -0300

    refactoring

--- Makefile
+++ Makefile
@@ -1,14 +1,8 @@
-stop:
-	docker-compose stop
-
-cleanup: stop
-	docker-compose rm -f -v
-
 run:
 	docker-compose up
 
 check:
-	docker-compose run --entrypoint "coverage run number_to_text/number_to_text_tests.py" webapp
+	docker-compose run --entrypoint "coverage run number_to_text/number_to_text_tests.py" challenge-webapp
 
 shell:
 	docker-compose run --entrypoint sh webapp
--- http_server.py
+++ http_server.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, jsonify
 
-from number_to_text import number_to_text
+from number_to_text.number_to_text import NumberToText
 
 app = Flask(__name__)
 
 @app.route('/<number>')
 def show_number_to_text(number):
-    parser = number_to_text.NumberToText()
+    parser = NumberToText()
     result = parser.number_to_text(int(number))
     json_result = jsonify({ "extenso": result })
     return json_result
